a33392cb21877cbbe3e319e03890de0b50184874
Maadhav Sharma <maadhav2001@gmail.com>
2021-07-20 13:02:28 +0530

    CodeNFTContract created

--- contracts/CodeNFTContract.sol
+++ contracts/CodeNFTContract.sol
@@ -1,7 +1,6 @@
-// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
 
-contract SimpleStorage {
+contract CodeNFTContract {
     event StorageSet(string _message);
 
     uint256 public storedData;
