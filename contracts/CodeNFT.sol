fadd9931ff58123c6f759fda98ed27163c016670
Maadhav Sharma <maadhav2001@gmail.com>
2021-07-22 14:10:02 +0530

    roles auth

--- contracts/CodeNFT.sol
+++ contracts/CodeNFT.sol
@@ -2,23 +2,42 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract CodeNFT is ERC721Enumerable {
+contract CodeNFT is Context, AccessControlEnumerable, ERC721Enumerable {
     using Strings for uint256;
     mapping(uint256 => string) private _tokenURIs;
 
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+     using Counters for Counters.Counter;
 
-    constructor() ERC721("Code", "CODE") {}
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    Counters.Counter private _tokenIdTracker;
+
+    string private _baseTokenURI;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseTokenURI
+    ) ERC721(name, symbol) {
+        _baseTokenURI = baseTokenURI;
+
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+    }
 
     function mintToken(address client, string memory repoURI)
         public
         returns (uint256)
     {
-        _tokenIds.increment();
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have minter role to mint");
+        _tokenIdTracker.increment();
 
-        uint256 id = _tokenIds.current();
+        uint256 id = _tokenIdTracker.current();
         _safeMint(client, id);
         _setTokenURI(id, repoURI);
 
@@ -67,5 +86,14 @@ contract CodeNFT is ERC721Enumerable {
         if (bytes(_tokenURIs[tokenId]).length != 0) {
             delete _tokenURIs[tokenId];
         }
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerable, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
