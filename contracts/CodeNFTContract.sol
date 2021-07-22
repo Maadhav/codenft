e8ec8f18711a8971d3c2bd8fc90c7ea275ee6db9
Maadhav Sharma <maadhav2001@gmail.com>
2021-07-22 11:28:20 +0530

    codenft contract updated

--- contracts/CodeNFTContract.sol
+++ contracts/CodeNFTContract.sol
@@ -1,14 +1,16 @@
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CodeNFTContract is ERC721URIStorage {
+contract CodeNFTContract is ERC721Enumerable {
+    using Strings for uint256;
+    mapping(uint256 => string) private _tokenURIs;
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Code", "CODE") {
-    }
+    constructor() ERC721("Code", "CODE") {}
 
     function mintToken(address client, string memory repoURI)
         public
@@ -21,5 +23,49 @@ contract CodeNFTContract is ERC721URIStorage {
         _setTokenURI(id, repoURI);
 
         return id;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI query for nonexistent token"
+        );
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
     }
 }
